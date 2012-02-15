1fb89ef75b823e699b1b2d3763c305a63f63d59f
amardaxini <amardaxini@gmail.com>
2012-02-15 20:17:44 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MakeMyCv::Application.config.secret_token = 'a61b71d751256736b04d1a1f27e5c32a0c788820c6e8a54179be315a086bbe73ed4d14b845a5b9fe19e6ccd9605d97b5f893f5db856c3ec5d0108be18fc2d44e'
