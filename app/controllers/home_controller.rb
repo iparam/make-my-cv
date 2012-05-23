01d82ce7814c853a273a9d1002a3bde1f50310d9
amardaxini <amardaxini@gmail.com>
2012-05-23 17:04:31 +0530

    fixed about us page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
- # skip_before_filter :authenticate_user!
+  skip_before_filter :authenticate_user!
   def index
-    binding.pry
    redirect_to profile_url if current_user
     
     
